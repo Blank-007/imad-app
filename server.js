fd89cf86ef9bd8856bd8def1b27f76d068542f7c
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-17 19:33:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,25 +6,43 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    
-    
-}
+    'article-one': {
+        title : 'Ariticle One',
+        date : 'Dec 14,2018',
+        heading : 'First'
+    },
+    'article-two': {
+        title : 'Ariticle two',
+        date : 'Dec 17,2018',
+        heading : 'Second'
+    },
+    'article-three': {
+        title : 'Ariticle three',
+        date : 'Dec 20,2018',
+        heading : 'Third'
+    }
+};
 
 function CreateTemplate(data){
     var title = data.title;
     var heading = data.heading;
+    var date = data.date;
+    
     var htmltemplate = ` 
     <html>
         <head>
             <title>
-                Article One | Blank
+                ${title} | Blank
             </title>
             <meta name="viewport" content="width=device-width , initial-scacle=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
             <div class="container">
                 <a href="/">Home</a>
-                <h4>This is the first article</h4>
+                <h2>${heading}</h2>
+                <p>${date}</p>
+                <h4>This is the content</h4>
             </div>    
         </body>    
     </html>`;
@@ -34,16 +52,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function(req,res){
-    res.send("Article two will be served here");
-});
-
-app.get('/article-three', function(req,res){
-    res.send("Article three will be served here");
+app.get('/:articleName', function(req,res){
+    var articleName = req.params.articleName;
+    res.send(CreateTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
