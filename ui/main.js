954aaf36f6af51f1dec678b25b15ee21f7a9aa0d
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-18 14:30:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@ submit.onclick = function () {
     
     
     
-    var names = ['name1','name2','name3'];
+    var names = ['name1','name2','name3','name4'];
     var list='';
     for (var i=0;i<names.length;i++){
         list += '<li>' + name[i] +'<li>';
