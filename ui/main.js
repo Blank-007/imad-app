f9eabca3f31cb8af5c7545097967d8188e6119f3
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-18 14:06:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,23 @@ img.onclick = function () {
     var interval = setInterval(moveRight,100);
 }
 
+var button = document.getElementById('counter');
+button.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+         
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }            
+        }
+    };
+    request.open('GET','http://mvabhinav1998.imad.hasura-app.io/counter',true);
+    request.send(null);
+
+} ;
