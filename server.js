56bf1bd586a8238adae01397f60c86ca9b19f15d
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-18 20:45:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/submit-name', function (req,res){
     res.send(JSON.stringify(names));
 });
 app.get('/articles/:articleName', function (req,res) {
-    var articleName = req.params.articleName;
+    
     pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName + "'" ,function (err,result){
        if(err){
            res.status(500).send(err.toString());
