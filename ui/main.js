9d42bb95a130fc96f93a685b8aaeecdb69c35f39
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-18 13:53:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 var img = document.getElementById('image');
 var marginleft = 0;
-function marginRight () {
+function moveRight () {
     marginleft = marginleft +10;
     image.style.marginLeft(marginleft + 'px');
 }
