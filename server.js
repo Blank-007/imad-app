45088f09a53de763dfe3b9485cbd7eb32e77b5ad
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-20 18:47:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedsting);
 }); 
 
-app.get('/create-user',function(req,res){
+app.post('/create-user',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
