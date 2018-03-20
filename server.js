b819d65d4bc9b4e9ea48ce98f03c5da4fe5d48a9
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-03-20 18:34:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool =require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user : 'mvabhinav1998',
@@ -16,6 +17,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
@@ -25,6 +27,20 @@ app.get('/hash/:input',function(req,res){
     var hashedstring = hash(req.params.input,'this is some randome text');
     res.send(hashedsting);
 }); 
+
+app.get('/create-user',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbstring = hash(password,salt);
+    pool.query('INSERT INTO "user (username,password) VALUES ($1,$2)',[username,dbstring],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send('User succesfully created' + username);
+       }
+    });
+});
 
 var pool = new Pool(config);
 function CreateTemplate(data){
