bd11397dbed71c509a9e5f7570f0e0010ccfd6e5
Abhinav MV <35369843+Blank-007@users.noreply.github.com>
2018-04-21 12:39:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,61 +1,23 @@
-console.log('Loaded!');
-
-var img = document.getElementById('image');
-var marginleft = 0;
-function moveRight () {
-    marginleft = marginleft +5;
-    img.style.marginLeft = marginleft+"px";
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight,100);
-}
-
-/*var button = document.getElementById('counter');
-button.onclick = function(){
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-         
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                
-            }            
-        }
-    };
-    request.open('GET','http://mvabhinav1998.imad.hasura-app.io/counter',true);
-    request.send(null);
-
-} ;*/
-
-
-var submit = document.getElementById('click');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function () {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list='';
-                for (var i=0;i<names.length;i++){
-                    list += '<li>' + names[i] +'</li>';
-                }
-                var ul=document.getElementById('ulist');
-                ul.innerHTML = list;
-                
-            }            
+                console.log('user logged in');
+                alert('Logged in');
+            } else if(request.status === 403) {
+                alert('username/password is incorrect');
+            } else if(request.status === 500) {
+                alert('something went wrong');
+            }          
         }
     };
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://mvabhinav1998.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST','http://mvabhinav1998.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(JSON.stringify({username: username,password: password}));
     
 
 };
